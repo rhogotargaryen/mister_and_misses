15f894f4de93fa49addbb892e627f2e046370b75
EC2 Default User <ec2-user@ip-172-31-32-154.us-east-2.compute.internal>
2018-04-13 06:43:33 +0000

    broken db connection WIP

--- Rakefile
+++ Rakefile
@@ -1,6 +1,4 @@
-ENV["SINATRA_ENV"] ||= "development"
-
-require_relative './config/environment'
+require './config/environment'
 require 'sinatra/activerecord/rake'
 
 task :console do
--- config/environment.rb
+++ config/environment.rb
@@ -1,10 +1,10 @@
+ENV['SINATRA_ENV'] ||= "development"
 
 require 'bundler/setup'
-Bundler.require(:default)
-
-ActiveRecord::Base.establish_connection(
-  :adapter => "sqlite3",
-  :database => "db/misters.sqlite"
-)
+Bundler.require(:default, ENV['SINATRA_ENV'])
 
 require_all './app'
+
+configure :development do
+  set :database, 'sqlite3:db/misters.db'
+end
