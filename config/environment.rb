407174b959f04362c7f1bef8dacdf419cb63bc50
EC2 Default User <ec2-user@ip-172-31-32-154.us-east-2.compute.internal>
2018-04-13 07:04:18 +0000

    DB connection not as broke as I thought works...sorta

--- config/environment.rb
+++ config/environment.rb
@@ -2,9 +2,7 @@ ENV['SINATRA_ENV'] ||= "development"
 
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
-
 require_all './app'
-
 configure :development do
   set :database, 'sqlite3:db/misters.db'
 end
