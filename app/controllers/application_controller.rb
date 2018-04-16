5c19a66a284ae5c838b72e397ec618b47b5858fc
EC2 Default User <ec2-user@ip-172-31-32-154.us-east-2.compute.internal>
2018-04-16 19:57:45 +0000

    thanks Rozelle for testing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,19 +16,13 @@ class ApplicationController < Sinatra::Base
   end
   
   post '/signup' do
-    @error = 
-    params.map do |k,v| 
-      if v == ""
-        k
-      end
-    end.compact
-    if !@error.empty?
-      erb :'/error'
-    elsif !User.find_by_username(params[:username])
+    validate
+    if !User.find_by_username(params[:username])
       User.create(params)
     elsif User.find_by_username(params[:username])
-      erb :'/signup_login'
+      erb :signup_login
     end
+    erb :home
   end
   
   get '/login' do
@@ -45,7 +39,43 @@ class ApplicationController < Sinatra::Base
   end
   
   get '/story' do
+    @couple_story = Couple.first.story
     erb :story
   end
+  
+  helpers do
+    
+    def validate
+      params[:username].downcase!
+      @errors = params.map do |k,v| 
+        k if v == ""  #refractor .blank?
+      end.compact
+      if !@errors.empty?
+        erb :errors
+      end
+    end
+    
+    def logged_in?
+        !!current_user
+    end
+    
+    def log_out
+        session.destroy
+    end
+    
+    def current_user
+      @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+    end
+    
+    def is_admin
+      logged_in?
+      if !!@current_user.couple
+        @@path = "/couple"
+      elsif logged_in?
+        @@path = "/users"
+      else
+        @@path = ""
+      end
+    end
+  end
 end
-
